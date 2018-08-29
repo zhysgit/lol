347b071c7f60c547f55459bff387fea7bc311eab
zhysgit <35164194+zhysgit@users.noreply.github.com>
2018-08-29 11:12:38 +0800

    Add files via upload

---
+++ LOL.cmd
@@ -0,0 +1,32 @@
+@echo off
+echo 九旬老太为何裸死街头？
+ping 127.0.0.1 -n 4 >nul
+cls
+echo 数百头母驴为何半夜惨叫？
+ping 127.0.0.1 -n 4 >nul
+cls
+echo 小卖部安全套为何屡遭黑手？
+ping 127.0.0.1 -n 4 >nul
+cls
+echo 女生宿舍内裤为何频频失窃？ 
+ping 127.0.0.1 -n 4 >nul
+cls
+echo 连环强奸母猪案，究竟是何人所为？
+ping 127.0.0.1 -n 4 >nul
+cls
+echo 老尼姑的门夜夜被敲，究竟是人是鬼？
+ping 127.0.0.1 -n 4 >nul
+cls
+echo 数百只小母狗意外身亡的背后又隐藏着什么？
+ping 127.0.0.1 -n 5 >nul
+cls
+echo 这一切的背后,是人性的扭曲还是道德的沦丧？
+ping 127.0.0.1 -n 5 >nul
+cls
+echo 是性的爆发还是饥渴的无奈？
+ping 127.0.0.1 -n 5 >nul
+cls
+echo 敬请关注今晚8点CCAV8年度巨献《楼下的不归之路》 让我们跟随着镜头走进变态狂的内心世界……
+color fc
+ping 127.0.0.1 -n 7 >nul
+exit
